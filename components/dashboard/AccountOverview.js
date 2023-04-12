18d93ef4045e7c4ad5d573918095c4808db72bf9
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-04-12 09:37:28 +0800

    allow member to edit profile

--- components/dashboard/AccountOverview.js
+++ components/dashboard/AccountOverview.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 
 function AccountOverview({
@@ -10,6 +11,7 @@ function AccountOverview({
 }) {
   const { email, firstName, lastName, contactNumber, address, facebookName } =
     data;
+  const [showUpdateModal, setShowUpdateModal] = useState(null);
 
   const UserApplications = () => {
     return (
@@ -246,13 +248,164 @@ function AccountOverview({
     );
   };
 
+  const UpdateModal = ({
+    setShowUpdateModal,
+    currFirstName,
+    currLastName,
+    currContactNumber,
+    currAddress,
+    currFacebookName,
+  }) => {
+    const [firstName, setFirstName] = useState(currFirstName);
+    const [lastName, setLastName] = useState(currLastName);
+    const [contactNumber, setContactNumber] = useState(currContactNumber);
+    const [address, setAddress] = useState(currAddress);
+    const [facebookName, setFacebookName] = useState(currFacebookName);
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const jwt = localStorage.getItem("accessToken");
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/users/` + email,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            contactNumber,
+            address,
+            facebookName,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      );
+
+      if (res.ok) {
+        console.log(res);
+        window.location.reload();
+      }
+    };
+
+    return (
+      <div
+        className={`fixed inset-0 flex items-center justify-center`}
+        style={{ background: "rgba(50, 50, 50, 0.8)" }}
+      >
+        <form
+          className="bg-white p-16 rounded-2xl relative"
+          onSubmit={handleSubmit}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => setShowUpdateModal(false)}
+            className="absolute top-6 right-6 hover:cursor-pointer"
+          >
+            <path
+              d="M16.3394 9.32245C16.7434 8.94589 16.7657 8.31312 16.3891 7.90911C16.0126 7.50509 15.3798 7.48283 14.9758 7.85938L12.0497 10.5866L9.32245 7.66048C8.94589 7.25647 8.31312 7.23421 7.90911 7.61076C7.50509 7.98731 7.48283 8.62008 7.85938 9.0241L10.5866 11.9502L7.66048 14.6775C7.25647 15.054 7.23421 15.6868 7.61076 16.0908C7.98731 16.4948 8.62008 16.5171 9.0241 16.1405L11.9502 13.4133L14.6775 16.3394C15.054 16.7434 15.6868 16.7657 16.0908 16.3891C16.4948 16.0126 16.5171 15.3798 16.1405 14.9758L13.4133 12.0497L16.3394 9.32245Z"
+              fill="currentColor"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
+              fill="currentColor"
+            />
+          </svg>
+          <p className="font-bold text-xl">Edit Profile</p>
+          <div className="grid gap-4 py-8">
+            <div className="grid grid-cols-2">
+              <p>First Name</p>
+              <input
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                placeholder="First Name"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Last Name</p>
+              <input
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                placeholder="Last Name"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Contact Number</p>
+              <input
+                type="text"
+                onChange={(e) => setContactNumber(e.target.value)}
+                value={contactNumber}
+                placeholder="Contact Number"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Address</p>
+              <input
+                type="text"
+                onChange={(e) => setAddress(e.target.value)}
+                value={address}
+                placeholder="Address"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Facebook Name</p>
+              <input
+                type="text"
+                onChange={(e) => setFacebookName(e.target.value)}
+                value={facebookName}
+                placeholder="Facebook Name"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-shpccRed text-[#fffffe] px-4 py-2 rounded-lg hover:shadow-lg uppercase"
+            >
+              Edit Profile
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
   return (
     <div>
+      {showUpdateModal && (
+        <UpdateModal
+          setShowUpdateModal={setShowUpdateModal}
+          currFirstName={firstName}
+          currLastName={lastName}
+          currContactNumber={contactNumber}
+          currAddress={address}
+          currFacebookName={facebookName}
+        />
+      )}
       <div className="bg-white p-8 rounded-3xl">
         <div className="flex justify-between items-center">
           <p className="font-bold text-3xl pb-4">Profile Overview</p>
           <div className="flex gap-4">
-            <div className="bg-gray-200 text-black p-2 px-6 rounded-lg hover:bg-gray-300 active:bg-gray-400 transition duration-200 text-center">
+            <div
+              className="bg-gray-200 text-black p-2 px-6 rounded-lg hover:bg-gray-300 active:bg-gray-400 transition duration-200 text-center hover:cursor-pointer"
+              onClick={() => setShowUpdateModal(true)}
+            >
               Edit Profile
             </div>
             <div className="bg-gray-200 text-black p-2 px-6 rounded-lg hover:bg-gray-300 active:bg-gray-400 transition duration-200 text-center">
